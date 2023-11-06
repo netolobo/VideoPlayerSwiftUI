51a4620fa67f952e4cb3fa566cfa2b286a3767ab
netolobo <desilio@gmail.com>
2023-11-05 21:07:32 -0800

    Initial commit

---
+++ VideoPlayerSwiftUI/VideoPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoPlayerView.swift
+//  VideoPlayerSwiftUI
+//
+//  Created by Neto Lobo on 05/11/23.
+//
+
+import SwiftUI
+
+struct VideoPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoPlayerView()
+}
