30c148f1c44507c325f56f9024340c89955a60bb
netolobo <desilio@gmail.com>
2023-11-05 20:13:27 -0800

    Initial Commit

---
+++ VideoPlayerSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoPlayerSwiftUI
+//
+//  Created by Neto Lobo on 05/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoPlayerSwiftUI/VideoPlayerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerSwiftUIApp.swift
+//  VideoPlayerSwiftUI
+//
+//  Created by Neto Lobo on 05/11/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
